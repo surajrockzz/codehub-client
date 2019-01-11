6263b161c62fa8bdfccf4df4e2c5707189f27501
surajrockzz <penugondasuraj@gmail.com>
2019-01-11 14:09:25 +0530

    signup css changed

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -12,39 +12,40 @@ class SignUp extends Component{
             email:'',
             password:''
         }
-        this.setUsername = this.setUsername.bind(this)
-        this.setname = this.setname.bind(this)
-        this.setemail = this.setemail.bind(this)
-        this.setdob = this.setdob.bind(this)
-        this.setPassword = this.setPassword.bind(this)
+        this.setValues = this.setValues.bind(this)
         this.submitDetails = this.submitDetails.bind(this)
     }
-    setUsername(e){
-        this.setState({
-            username:e.target.value,
-        })
+
+    setValues(e){
+        if(e.target.id==="username"){
+            this.setState({
+                username: e.target.value
+            })
+        }
+        else if (e.target.id === "email"){
+            this.setState({
+                email: e.target.value
+            })
+        }
+        else if (e.target.id === "name"){
+            this.setState({
+                name: e.target.value
+            })
+        }
+        else if (e.target.id === "dob"){
+            this.setState({
+                dob: e.target.value
+            })
+        }
+        else if(e.target.id === "Password"){
+            this.setState({
+                password: e.target.value
+            });
+        }
     }
-    setname(e) {
-        this.setState({
-            name: e.target.value,
-        })
-    }
-    setemail(e) {
-        this.setState({
-            email: e.target.value,
-        })
-    }
-    setPassword(e) {
-        this.setState({
-            password: e.target.value,
-        })
-    }
-    setdob(e) {
-        this.setState({
-            dob: e.target.value,
-        })
-    }
+    
     submitDetails(){
+        console.log(this.state)
         axios.post(urls.apisignup, {
                 date_of_birth:this.state.dob,
                 email: this.state.email,
@@ -70,23 +71,23 @@ class SignUp extends Component{
                          <h3>SignUp</h3>
                          <FormGroup>
                             <Label for="username">Username</Label>
-                            <Input type="text" name="username" id="username" placeholder="Enter username" onChange={this.setUsername} value={this.state.username}/>
+                            <Input type="text" name="username" id="username" placeholder="Enter username" onChange={this.setValues} value={this.state.username}/>
                         </FormGroup>
                         <FormGroup>
                             <Label for="name">Name</Label>
-                            <Input type="text" name="name" id="name" placeholder="Enter name" onChange={this.setname} value={this.state.name}/>
+                            <Input type="text" name="name" id="name" placeholder="Enter name" onChange={this.setValues} value={this.state.name}/>
                         </FormGroup>
                         <FormGroup>
                             <Label for="name">Date of Birth</Label>
-                            <Input type="date" name="dob" id="dob" placeholder="Enter dob" onChange={this.setdob} value={this.state.dob}/>
+                            <Input type="date" name="dob" id="dob" placeholder="Enter dob" onChange={this.setValues} value={this.state.dob}/>
                         </FormGroup>
                         <FormGroup>
                             <Label for="Email">Email</Label>
-                            <Input type="email" name="email" id="Email" placeholder="Enter email" onChange={this.setemail} value={this.state.email}/>
+                            <Input type="email" name="email" id="email" placeholder="Enter email" onChange={this.setValues} value={this.state.email}/>
                         </FormGroup>
                         <FormGroup>
                             <Label for="Password">Password</Label>
-                            <Input type="password" name="password" id="Password" placeholder="Enter password" onChange={this.setPassword} value={this.state.password} />
+                            <Input type="password" name="password" id="Password" placeholder="Enter password" onChange={this.setValues} value={this.state.password} />
                         </FormGroup>
 
                         <Button color="primary" onClick={this.submitDetails}>Start Coding Now</Button>
